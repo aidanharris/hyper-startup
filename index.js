8eed448f629c9639706c9116222e4b66cead4371
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2017-01-05 18:02:55 +0000

    Change implementation so that a new session will also have the commands executed.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-let fs = require('fs');
-let loaded = false;
+// let fs = require('fs');
 let commands = [];
 
 // debugging purposes
@@ -9,16 +8,11 @@ let commands = [];
 // 	fs.appendFile(`${__dirname}/log.txt`, str + '\n');
 // }
 
-function waitForSessions(sessions) {
-	if (!loaded) {
-		sessions.forEach(session => {
-			commands.forEach(cmd => {
-				session.write(cmd + '\r');
-			});
-			loaded = true;
-		});
-
-		setTimeout(() => waitForSessions(sessions), 10);
+function waitFor(object, key, fn) {
+	if (key in object) {
+		fn(object[key]);
+	} else {
+		setTimeout(() => waitFor(object, key, fn), 10);
 	}
 }
 
@@ -29,6 +23,19 @@ exports.onApp = function (obj) {
 	}
 };
 
-exports.onWindow = function (win) {
-	waitForSessions(win.sessions);
+exports.onWindow = win => {
+	win.rpc.on('execute commands', uid => {
+		commands.forEach(cmd => {
+			win.sessions.get(uid).write(cmd + '\r');
+		});
+	});
+};
+
+exports.onRendererWindow = win => {
+	waitFor(win, 'rpc', rpc => {
+		rpc.on('session add', details => {
+			const { uid } = details;
+			rpc.emit('execute commands', uid);
+		});
+	});
 };
