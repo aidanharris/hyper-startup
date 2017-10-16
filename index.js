75cbf9c93f18b1a26c26e8c61f99fde5ab6bd762
Aidan Harris <mail@aidanharris.io>
2017-10-16 09:48:50 +0100

    Add -e flag

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // let fs = require('fs');
 let commands = [];
+const exec = process.argv.indexOf('-e');
 
 // debugging purposes
 // function log(str) {
@@ -20,6 +21,12 @@ exports.onApp = function (obj) {
 	const config = obj.config.getConfig();
 	if (config.commands) {
 		commands = config.commands;
+	}
+	if (exec !== -1 && process.argv[exec+1]) {
+		commands = [process.argv[exec+1]];
+		if (process.argv.indexOf('--hold') === -1 && process.argv.indexOf('--noclose') === -1) {
+			commands[0]+=';exit $?';
+		}
 	}
 };
 
